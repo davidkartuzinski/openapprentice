cd19615890a39540938216c573f09e36decdb127
davidkartuzinski <davidk@kaidawei.com>
2019-07-16 09:52:38 +0200

    fixed file names

---
+++ src/components/comingSoon.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const ComingSoon = () => {
+  return (
+    <div
+      style={{
+        color: "red",
+        display: "block",
+        letterSpacing: "14px",
+        width: "85%",
+        margin: "0 auto",
+        textAlign: "center",
+        marginBottom: 0,
+        fontSize: "2.5rem",
+        fontFamily: "sans-serif",
+        marginTop: "2rem",
+      }}
+    >
+      COMING SOON
+    </div>
+  )
+}
+
+export default ComingSoon
